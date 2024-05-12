fa6929cf279c70a9be187d2a416904bbdb095190
Abdelrahman Ragab <Abdelrhmanr2022@gmail.com>
2024-05-12 22:36:57 +0300

    Add DataFrame Solution

--- Count Words/Count Tokens.py
+++ Count Words/Count Tokens.py
@@ -5,6 +5,7 @@ Created on Sat May 11 12:11:09 2024
 @author: Abdelrahman Ragab
 """
 
+
 # Importing necessary libraries
 import pyspark
 from pyspark.sql import SparkSession
@@ -37,3 +38,22 @@ rdd3 = rdd2.reduceByKey(lambda x,y : x+y)
 
 # Collecting and printing the final word count
 rdd3.collect()
+
+
+
+#################################################################
+
+
+# Adding DataFrame Solution 
+
+# Create DataFrame from rdd2
+df = spark.createDataFrame(rdd2.collect(), ['word','freq'])
+
+# PRINT SCHEMA 
+
+print(df.printSchema())
+
+# AGG Sum of each words and return only first 10
+df.groupBy('word').sum('freq').head(10)
+
+
